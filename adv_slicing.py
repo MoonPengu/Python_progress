cc74d1fad0612417c8dad8550812e753fa1a6e6a
MoonPengu <63443127+MoonPengu@users.noreply.github.com>
2021-11-13 23:15:49 +0530

    The advance slicing

---
+++ adv_slicing.py
@@ -0,0 +1,5 @@
+mystr = "Shreya is aweeesomeee !!!"
+print(mystr[0::2]) # Take it in rough as [0:25:2] --> will print the string in the gap of 2
+print(mystr[0:]) # Print the whole string
+print(mystr[0:10:2]) # Print from 0 to 10 in gap of 2
+print(len(mystr))
