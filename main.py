cdf21c3c4d6c5fb47b191fc2745d64096b970881
MoonPengu <63443127+MoonPengu@users.noreply.github.com>
2021-11-13 20:53:56 +0530

    Sum of two numbers using user input

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print("Enter your number :")
-inpnum = int(input())
-print(input(), inpnum+10)
+print("Enter your 1st number :")
+inpnum1 = int(input())
+print("Enter your 2nd number :")
+inpnum2 = int(input())
+
+sum = inpnum1 + inpnum2
+print(sum)
