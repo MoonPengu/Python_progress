733344aad7cfb53b996da39b750b18d722fecb1c
MoonPengu <63443127+MoonPengu@users.noreply.github.com>
2021-11-16 09:00:20 +0530

    Exercise on creating my own dictionary
    Create a dictionary of 3 to 4 words, and let the user type a word and the meaning of it will get printed.

---
+++ Dict_exercise.py
@@ -0,0 +1,5 @@
+print("Welcome to apna dictionary !!")
+d1 = {"Mango":"It is a fruit", "variable":"reserved memory location to store values", "array":"collection of items stored at contiguous memory locations"}
+dictionary = input("Enter the word you want the meaning : ")
+print(d1.get(dictionary))
+
